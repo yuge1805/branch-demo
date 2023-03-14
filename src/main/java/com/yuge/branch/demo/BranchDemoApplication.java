785bd9889f6ba7e61896d2d6672b7c1d9ece17a5
zhangbw <yuge1805@yeah.net>
2023-03-14 10:12:54 +0800

    feat: init

---
+++ src/main/java/com/yuge/branch/demo/BranchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yuge.branch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchDemoApplication.class, args);
+	}
+
+}
